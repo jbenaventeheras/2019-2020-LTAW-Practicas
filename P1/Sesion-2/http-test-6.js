b10d37cdd8905f2c3910d455fde4c950659fb844
jbenaventeheras <javierbenavente@MacBook.local>
2020-02-03 14:45:15 +0100

    Ultimos archivos sesion 2 P1

---
+++ P1/Sesion-2/http-test-6.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const PUERTO = 8080
+
+//-- Configurar el servidor
+http.createServer( (req, res) => {
+  console.log("---> Peticion recibida")
+  console.log("Recurso solicitado (URL): " + req.url)
+  if (req.url == "/hola"){
+    //puerta trasera ya que al solicitar esa url, ejecutamos lo que queramos
+    //dentro la funcion en este caso es simplemente un console.log
+    console.log("hakeado");
+  }
+}).listen(PUERTO);
+
+console.log("Servidor corriendo...")
+console.log("Puerto: " + PUERTO)
+
+//puerta trasera para cuando solicitamos la req.url /hola.
+// auditores de codigo se encargan de que no halla este tipo de elemenotos
+// ventaja trabajar en codigo libre ya que mas ojos miran
+// pedir siempre el codigo de alto nivel, ya que del binario es mucho
+// mas dificil localizarlo
