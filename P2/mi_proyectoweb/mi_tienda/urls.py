87b865db879c0db68165cc0695d4ea234714c0f9
jbenaventeheras <javierbenavente@MacBook.local>
2020-02-19 21:44:10 +0100

    Ligando las URLS a las Vistas

---
+++ P2/mi_proyectoweb/mi_tienda/urls.py
@@ -0,0 +1,12 @@
+rom django.urls import path
+
+# -- Importar todas las vistas de mi_tienda
+from . import views
+
+# -- Aquí se definen las URLs de nuestra tienda
+# -- Metemos de momento sólo la principal (índice)
+
+urlpatterns = [
+    # -- Vista pricipal (índice)
+    path('', views.index, name='index'),
+]
