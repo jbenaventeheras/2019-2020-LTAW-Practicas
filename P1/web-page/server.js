01526172d1f88501f6af3a8530caa93968cf18b9
jbenaventeheras <javierbenavente@MacBook.local>
2020-04-06 22:39:06 +0200

    servidor para text css e imanges

--- P1/web-page/server.js
+++ P1/web-page/server.js
@@ -1,45 +1,66 @@
 const http = require('http');
 const url = require('url');
 const fs = require('fs');
-const PUERTO = 8080
+const path = require('path')
+const PUERTO = 8080;
+const LOCALIP = '192.168.1.37';
+var express = require('express')
+var favicon = require('serve-favicon')
+
+var app = express()
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
 //-- Configurar el servidor
-http.createServer( (req, res) => {
-  console.log("---> Peticion recibida")
-  console.log("Recurso solicitado (URL): " + req.url)
-  var q = url.parse(req.url, true);
-  console.log("Recurso solicitado (URL): " + q)
-  console.log("URL parseada: ")
-  console.log("Host: " + q.host)
-  console.log("pathname:" + q.pathname)
+http.createServer((req, res) => {
 
-  //_- Crear el mensaje de respuesta. Primero la cabecera
-  //-- El código 200 se usa para indicar que todo está ok
-  //-- En el campo Content-Type tenemos que introducir el tipo MIME
-  //-- de lo que devolvemos
-  let mime = "text/plain"
-  res.writeHead(200, {'Content-Type': mime});
 
-  //-- Ahora escribimos el cuerpo: los datos a enviar, en html
-  //-- En este ejemplo se envia directamente el HTML
-  let msg = `
-   <!DOCTYPE html>
-  <html lang="es" dir="ltr">
-    <head>
-      <meta charset="utf-8">
-      <title>Mi tienda</title>
-    </head>
-    <body>
-      <p> ¡BIENVENIDO A MI TIENDA! </p>
-    </body>
-  </html>
-  `
-  //-- Meter el mensaje en la respuesta
-  res.write(msg);
 
-  //-- Ahora terminamos la respuesta, invocando el método end
-  res.end();
+  console.log("----------> Peticion recibida")
+  let q = url.parse(req.url, true);
+  console.log("Recurso:" + q.pathname)
+
+
+  // creating generic index
+    var filename = ""
+    if (q.pathname == "/")
+      filename += "/index.html";
+    else {
+      filename = q.pathname;
+    }
+
+    type = filename.split(".")[1]
+    filename = "." + filename
+
+    console.log("Filename: " + filename);
+    console.log("Type: " + type);
+
+    var mime = "text/html"
+    fs.readFile(filename, (err, data) => {
+      if (err) {
+        res.writeHead(404, {'Content-Type': mime});
+        return res.end("404 Not Found " + q.pathname +
+                       ' but we will create it.');
+      }
+
+  // default mime type
+    var mime = "text/html"
+  // for images
+    if (['png', 'jpg'].includes(type)) {
+      console.log("LOADING IMAGE")
+      mime = "image/" + type;
+    }
+  // for css
+    if (type == "css"){
+      mime = "text/css";
+    }
+
+    res.writeHead(200, {'Content-Type': mime});
+    res.write(data);
+    res.end();
+    console.log('____________END REQUEST____________\n');
+  });
 
 }).listen(PUERTO);
+
 console.log("Servidor corriendo...")
 console.log("Puerto: " + PUERTO)
